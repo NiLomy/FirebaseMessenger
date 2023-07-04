194da1f2e454cf72c927af20e86eb5d8f330c7ab
Salavat <asnsalavat@gmail.com>
2023-07-04 14:28:45 +0300

    Переписал SignUpFragment, написал логику LoginFragment

--- app/src/main/java/ru/kpfu/itis/android/team22/firebasemessenger/SignUpFragment.kt
+++ app/src/main/java/ru/kpfu/itis/android/team22/firebasemessenger/SignUpFragment.kt
@@ -1,115 +1,131 @@
 package ru.kpfu.itis.android.team22.firebasemessenger
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
 import ru.kpfu.itis.android.team22.firebasemessenger.databinding.FragmentSignUpBinding
 
-class SignUpFragment: Fragment(R.layout.fragment_sign_up) {
-    private var binding: FragmentSignUpBinding? = null
+class SignUpFragment : Fragment(R.layout.fragment_sign_up) {
+    private var _binding: FragmentSignUpBinding? = null
+    private val binding get() = _binding!!
+    private var _auth: FirebaseAuth? = null
+    private val auth get() = _auth!!
+    private var context : Context? = null
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = FragmentSignUpBinding.bind(view)
+        _binding = FragmentSignUpBinding.bind(view)
+        _auth = Firebase.auth
+        context = requireContext().applicationContext
 
-        val context = requireContext().applicationContext
-        val auth = Firebase.auth
+        setUp()
+    }
 
-        val btnSignUp = binding?.btnSignUp
-        val etName = binding?.etName
-        val etEmail = binding?.etEmail
-        val etPassword = binding?.etPassword
-        val etConfirmPassword = binding?.etConfirmPassword
-        val btnLogin = binding?.btnLogin
+    private fun setUp() {
+        val btnSignUp = binding.btnSignUp
+        val etName = binding.etName
+        val etEmail = binding.etEmail
+        val etPassword = binding.etPassword
+        val etConfirmPassword = binding.etConfirmPassword
+        val btnLogin = binding.btnLogin
 
+        btnSignUp.setOnClickListener {
+            onSignUpClicked(etName.text.toString(), etEmail.text.toString(), etPassword.text.toString(), etConfirmPassword.text.toString())
+        }
 
-        btnSignUp?.setOnClickListener {
-            val userName = etName?.text.toString()
-            val email = etEmail?.text.toString()
-            val password = etPassword?.text.toString()
-            val confirmPassword = etConfirmPassword?.text.toString()
+        btnLogin.setOnClickListener {
+            findNavController().navigate(R.id.nav_from_signup_to_login)
+        }
+    }
 
-            if (userName.isEmpty()) {
-                Toast.makeText(context, "Username must be non-empty", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-            if (email.isEmpty()) {
-                Toast.makeText(context, "Email must be non-empty", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
+    private fun onSignUpClicked(userName: String, email: String, password: String, confirmPassword: String) {
+        if (userName.isEmpty()) {
+            showToast(getString(R.string.user_name_must_be_non_empty))
+            return
+        }
+        if (email.isEmpty()) {
+            showToast(getString(R.string.email_must_be_non_empty))
+            return
+        }
+        if (password.isEmpty()) {
+            showToast(getString(R.string.password_must_be_non_empty))
+            return
+        }
+        if (password.length < 6) {
+            showToast(getString(R.string.password_length_is_to_short))
+            return
+        }
+        if (confirmPassword.isEmpty()) {
+            showToast(getString(R.string.confirm_password))
+            return
+        }
+        if (password != confirmPassword) {
+            showToast(getString(R.string.passwords_dont_match))
+            return
+        }
 
-            if (password.isEmpty()) {
-                Toast.makeText(context, "Password must be non-empty", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
+        signUpUser(email, password, userName)
+    }
 
-            if (password.length < 6)  {
-                Toast.makeText(context, "Password must be longer than 6 characters", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
+    private fun signUpUser(email: String, password: String, userName: String) {
+        // TODO: если пароль слабый, то без объявления ошибки не регает пользователя - починить
+        // TODO: как-то уведомлять, если пользователь с такой почтой уже есть
+        // Initial task failed for action RecaptchaAction(action=signUpPassword)with exception - The given password is invalid
+        activity?.let { fragmentActivity ->
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(fragmentActivity) { task ->
+                    if (task.isSuccessful) {
+                        val user: FirebaseUser? = auth.currentUser
+                        val userId: String = user!!.uid
 
-            if (confirmPassword.isEmpty()) {
-                Toast.makeText(
-                    context,
-                    "Confirm your password!",
-                    Toast.LENGTH_SHORT
-                ).show()
-                return@setOnClickListener
-            }
+                        val databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(userId)
 
-            if (password != confirmPassword) {
-                Toast.makeText(context, "Passwords don't match...", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
+                        val hashMap: HashMap<String, String> = HashMap()
+                        hashMap["userId"] = userId
+                        hashMap["userName"] = userName
+                        hashMap["profileImage"] = ""
 
-            // TODO: если пароль слабый, то без объявления ошибки не регает пользователя - починить
-            // TODO: как-то уведомлять если пользователь с такой почтой уже есть
-            // Initial task failed for action RecaptchaAction(action=signUpPassword)with exception - The given password is invalid
-
-            activity?.let { fragmentActivity ->
-                auth.createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(fragmentActivity) {
-                        if (it.isSuccessful) {
-                            val user: FirebaseUser? = auth.currentUser
-                            val userId: String = user!!.uid
-
-                            val databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(userId)
-
-                            val hashMap: HashMap<String, String> = HashMap()
-                            hashMap.put("userId", userId)
-                            hashMap.put("userName", userName)
-                            hashMap.put("profileImage", "")
-                            Toast.makeText(context, "aaa!", Toast.LENGTH_SHORT).show()
-
-                            databaseReference.setValue(hashMap).addOnCompleteListener(fragmentActivity) {
-                                if (it.isSuccessful) {
-                                    etName?.setText("")
-                                    etEmail?.setText("")
-                                    etPassword?.setText("")
-                                    etConfirmPassword?.setText("")
-
-                                    // TODO: написать логику перехода на экран пользователя
-                                    Toast.makeText(context, "OK!", Toast.LENGTH_SHORT).show()
-                                }
-                            }
-                        }
+                        saveUserDataToDatabase(databaseReference, hashMap)
+                    } else {
+                        showToast(getString(R.string.check_email))
+                        binding.etEmail.error = ""
                     }
+                }
+        }
+    }
+
+    private fun saveUserDataToDatabase(databaseReference: DatabaseReference, hashMap: HashMap<String, String>) {
+        databaseReference.setValue(hashMap)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    clearFields()
+                    showToast(getString(R.string.registration_success))
+                    // TODO: написать логику перехода на экран пользователя
+                }
             }
+    }
 
-        }
+    private fun showToast(message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
 
-        // TODO: переписать на переход на фрагмент логина
-        btnLogin?.setOnClickListener {
-            Toast.makeText(context, "hello", Toast.LENGTH_SHORT).show()
-        }
+    private fun clearFields() {
+        binding.etName.setText("")
+        binding.etEmail.setText("")
+        binding.etPassword.setText("")
+        binding.etConfirmPassword.setText("")
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        binding = null
+        _binding = null
     }
 }
