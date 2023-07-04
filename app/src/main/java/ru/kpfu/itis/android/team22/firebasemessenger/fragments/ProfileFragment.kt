10e635a8677ac3edf65a559d0c92a1ec2de38f52
Salavat <asnsalavat@gmail.com>
2023-07-04 20:21:22 +0300

    Added logout button to ProfileFragment

--- app/src/main/java/ru/kpfu/itis/android/team22/firebasemessenger/fragments/ProfileFragment.kt
+++ app/src/main/java/ru/kpfu/itis/android/team22/firebasemessenger/fragments/ProfileFragment.kt
@@ -1,7 +1,33 @@
 package ru.kpfu.itis.android.team22.firebasemessenger.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import ru.kpfu.itis.android.team22.firebasemessenger.R
+import ru.kpfu.itis.android.team22.firebasemessenger.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    private var _auth: FirebaseAuth? = null
+    private val auth get() = _auth!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        _binding = FragmentProfileBinding.bind(view)
+        _auth = Firebase.auth
+
+        val btnLogOut = binding.btnLogOut
+
+        btnLogOut.setOnClickListener{
+            auth.signOut()
+            findNavController().navigate(R.id.nav_from_container_to_login)
+        }
+    }
 }
