0e3903965803a4b3174fadd96f83baa26cbccddd
Nikita Lobanov <114010651+NiLomy@users.noreply.github.com>
2023-07-05 20:12:38 +0300

    Revert "Task/minor changes"

--- app/src/main/java/ru/kpfu/itis/android/team22/firebasemessenger/entities/User.kt
+++
@@ -1,7 +0,0 @@
-package ru.kpfu.itis.android.team22.firebasemessenger.entities
-
-data class User(
-    val userId: String = "",
-    val userName: String = "",
-    val profileImage: String = "",
-)
