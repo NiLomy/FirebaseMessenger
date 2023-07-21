ac415ff87b0da3d6366149de295ce8a56ba0d282
Salavat <asnsalavat@gmail.com>
2023-07-21 18:10:48 +0300

    Added some views

---
+++ app/src/main/java/ru/kpfu/itis/android/team22/firebasemessenger/entities/Image.kt
@@ -0,0 +1,8 @@
+package ru.kpfu.itis.android.team22.firebasemessenger.entities
+
+data class Image(
+    var senderID: String = "",
+    var receiverID: String = "",
+    var imagePath: String = "",
+    var time: String = ""
+)
